9fa9a156dfbd37002734f2695132041262731275
cheesechoux <gsoosuu@gmail.com>
2023-06-30 18:05:42 +0900

    feat: HttpLoggingInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
+import { HttpLoggingInterceptor } from '@src/common/http-logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new HttpLoggingInterceptor());
+
   await app.listen(3001);
 }
 bootstrap();
